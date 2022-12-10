75e888a78d93ea1eab1ebe88de3818a9bfb5c028
Matias Yocca <5626046+mayocca@users.noreply.github.com>
2022-12-10 16:19:05 -0300

    Agregar configuracion de colores

---
+++ .config/nvim/after/plugin/colors.lua
@@ -0,0 +1,8 @@
+require("catppuccin").setup({
+    flavour = "mocha", -- latte, frappe, macchiato, mocha
+    transparent_background = true,
+    term_colors = true,
+})
+
+vim.cmd.colorscheme "catppuccin"
+
