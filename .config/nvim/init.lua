42581b1c13aa9dc8f3072529717c568c0eacf3fb
Matias Yocca <5626046+mayocca@users.noreply.github.com>
2022-12-31 15:38:12 -0300

    Modify neovim config structure

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,8 @@
-require('yocca')
+-- <<---------- Dependencies ---------->>
+
+-- ripgrep         - https://github.com/BurntSushi/ripgrep
+-- git             - https://git-scm.com/
+
+require('user.settings')
+require('user.plugins')
 
--- .config/nvim/lua/yocca/init.lua
+++
@@ -1,3 +0,0 @@
-require('yocca.set')
-require('yocca.packer')
-
